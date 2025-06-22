ce82d1fef54ca18d781dc4779d29130fe02aeb44
vasylzayachkivskyi <vasylzaya@gmail.com>
2025-06-22 20:46:43 +0200

    fix

---
+++ js/main.js
@@ -0,0 +1,115 @@
+$(document).ready(function () {
+
+    // Text printing animation
+    var $el = $('.animated-text');
+    var fullText = $el.text().trim();
+    var index = 0;
+    var reverse = false;
+    var delay = 100;
+
+    $el.text('');
+
+    function typeEffect() {
+        if (!reverse) {
+            if (index < fullText.length) {
+                $el.text(fullText.substring(0, index + 1));
+                index++;
+            } else {
+                reverse = true;
+                setTimeout(typeEffect, 1500);
+                return;
+            }
+        } else {
+            if (index > 0) {
+                $el.text(fullText.substring(0, index - 1));
+                index--;
+            } else {
+                reverse = false;
+                setTimeout(typeEffect, 1500);
+                return;
+            }
+        }
+
+        setTimeout(typeEffect, delay);
+    }
+
+    typeEffect();
+
+    // Header scrolling animation
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('.header').addClass('small');
+        } else {
+            $('.header').removeClass('small');
+        }
+    });
+
+    // section animation
+    if (document.querySelector('.gradient-bg')) {
+
+        const interBubbles = document.querySelectorAll('.interactive');
+
+        interBubbles.forEach((interBubble) => {
+            let curX = 0;
+            let curY = 0;
+            let tgX = 0;
+            let tgY = 0;
+
+
+            const move = () => {
+                curX += (tgX - curX) / 20;
+                curY += (tgY - curY) / 20;
+                interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
+                requestAnimationFrame(move);
+            };
+
+            window.addEventListener('mousemove', (event) => {
+                tgX = event.clientX;
+                tgY = event.clientY;
+            });
+
+            move();
+        });
+
+    }
+
+
+    //  Main slider
+    var swiper = new Swiper(".aboutslider", {
+        slidesPerView: 'auto',
+        spaceBetween: 20,
+        speed: 4000,
+        loop: true,
+
+        autoplay: {
+            delay: 0,
+            disableOnInteraction: false,
+        },
+
+    });
+
+    // Anchor
+    $('.anchor').on('click', function (e) {
+        e.preventDefault();
+        const target = $(this).attr('href');
+        const offsetTop = $(target).offset().top;
+
+        $('html, body').animate({
+            scrollTop: offsetTop,
+        }, 400);
+    })
+
+    // Mobile menu
+    $('.burger').on('click', function () {
+        $('.header__menu').toggleClass('active');
+        $(this).toggleClass('on');
+        $('body').toggleClass('on');
+    });
+    $('.header__menu a').on('click', function () {
+        $('.header__menu').removeClass('active');
+        $('.burger').removeClass('on');
+        $('body').removeClass('on');
+    });
+
+
+});
